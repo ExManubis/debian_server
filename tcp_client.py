45b4f519f419b11399e854bc9c1504ebb4ec871e
Mikkel Jørgensen <mikjorgensen@proton.me>
2023-10-20 08:52:40 +0200

    update

--- tcp_client.py
+++ tcp_client.py
@@ -1,21 +1,25 @@
 #! /bin/python3
 # IMPORTS
 import socket
-import time
+import sys
 
 # VARIABLES
 server_host = "127.0.0.1"
-server_port = 65433
+server_port = 65431
 
 # PROGRAM
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((server_host, server_port))
-    bruger = '\nObservant: ' + input('Dit navn: ')
-    while True:
-        tid = 'Tid: ' + input('Tid: ')
-        spiller = '\nSpiller: ' + input('Spiller: ')
-        obs = '\nObservation: ' + input('Observation: ')
-        message = tid + spiller + obs + bruger
-        s.sendto(message.encode(), (server_host, server_port))
-        print()
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_host, server_port))
+        bruger = '\nObservant: ' + input('Dit navn: ')
+        while True:
+            tid = 'Tid: ' + input('Tid: ')
+            spiller = '\nSpiller: ' + input('Spiller: ')
+            obs = '\nObservation: ' + input('Observation: ')
+            message = tid + spiller + obs + bruger
+            s.sendto(message.encode(), (server_host, server_port))
+            print()
+except KeyboardInterrupt:
+    print('quitting...')
+    sys.exit()
 
--- tcp_server.py
+++ tcp_server.py
@@ -1,23 +1,44 @@
 #! /bin/python3
 # IMPORTS
 import socket
+import sys
 
 # VARIABLES
+
+# set server IP and port
 server_host = '127.0.0.1'
-server_port = 65433
+server_port = 65431
+
+# open data file and make variable for appending data
+f = open('data.txt', 'a') # a = appending 
 
 # PROGRAM
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((server_host, server_port))
-    s.listen()
-    connection, addr = s.accept()
-    with connection:
-        print(f'Connected by {addr}')
-        #connection.setblocking(False)
-        while True:
-            message, client = connection.recvfrom(1024)
-            print()
-            print('===============================')
-            print(message.decode(), f'\nIP: {addr}')
-            print('===============================')
-            print()
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((server_host, server_port))
+        s.listen()
+        connection, addr = s.accept()
+        with connection:
+            print(f'Connected by {addr}')
+            #connection.setblocking(False)
+            while True:
+                message, client = connection.recvfrom(1024)
+                print()
+                print('===============================')
+                besked = message.decode() + f'\nIP: {addr}'
+                if besked == f'\nIP: {addr}':
+                    break
+                else:
+                    print(besked)
+                    print('===============================')
+                    print()
+                    f.write('==============================')
+                    f.write('\n')
+                    f.write(besked)
+                    f.write('\n')
+                    f.write('==============================')
+                    f.write('\n')
+                    f.close
+except KeyboardInterrupt:
+    print('quitting SSO....')
+    sys.exit()
